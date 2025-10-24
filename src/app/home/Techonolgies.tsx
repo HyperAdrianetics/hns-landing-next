f1726de531cda5a442ff534510799b73faae6c5d
Adrian Lona <104098971+AdrianLonaFragoso@users.noreply.github.com>
2025-10-23 19:50:32 -0600

    feat: Gradient technologies

--- src/app/home/Techonolgies.tsx
+++ src/app/home/Techonolgies.tsx
@@ -14,21 +14,26 @@ import aws from "../assets/technologies/aws.svg";
 const Techonolgies = () => {
   const techItems = [
     { src: react, alt: "React", selected: true },
-    { src: nodeJs, alt: "Node.js", selected: false },
-    { src: python, alt: "Python", selected: false },
-    { src: typescript, alt: "TypeScript", selected: false },
-    { src: aws, alt: "AWS", selected: false },
-    { src: docker, alt: "Docker", selected: false },
-    { src: postgresSql, alt: "PostgreSQL", selected: false },
-    { src: mongoDb, alt: "MongoDB", selected: false },
-    { src: nextJs, alt: "Next.js", selected: false },
-    { src: reactNative, alt: "React Native", selected: false },
+    { src: nodeJs, alt: "Node.js", selected: true },
+    { src: python, alt: "Python", selected: true },
+    { src: typescript, alt: "TypeScript", selected: true },
+    { src: aws, alt: "AWS", selected: true },
+    { src: docker, alt: "Docker", selected: true },
+    { src: postgresSql, alt: "PostgreSQL", selected: true },
+    { src: mongoDb, alt: "MongoDB", selected: true },
+    { src: nextJs, alt: "Next.js", selected: true },
+    { src: reactNative, alt: "React Native", selected: true },
   ];
   return (
-    <section id="technologies" className="min-h-screen p-5 container mx-auto">
+    <section
+      id="technologies"
+      className="min-h-screen p-5 pt-30 container mx-auto"
+    >
       <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-6 gap-0 w-full border border-[#383E36]">
-        <article className="col-span-2 md:col-span-2 lg:col-span-2 flex items-center justify-center ring-1 ring-inset ring-[#383E36] min-h-[230px]">
-          <h2 className="text-4xl font-bold mb-4 text-[var(--primaryYellow)]">
+        <article className="col-span-2 md:col-span-2 lg:col-span-2 flex items-center justify-center ring-1 ring-inset ring-[#383E36] min-h-[230px] relative ">
+          <div className="bg-[radial-gradient(circle_at_top_left,_rgba(63,183,36,0.15)_0%,_rgba(127,231,122,0.05)_30%,_rgba(6,8,17,1)_100%)] w-full h-full absolute top-0 left-0 z-0"></div>
+
+          <h2 className="text-4xl font-bold mb-4 text-[var(--primaryYellow)] z-10">
             Tecnologías <br />
             <span className="text-[var(--primaryGreen)]">que dominamos</span>
           </h2>
@@ -41,8 +46,8 @@ const Techonolgies = () => {
             {item.selected && (
               <div className="bg-[radial-gradient(circle_at_top_left,_rgba(63,183,36,0.15)_0%,_rgba(127,231,122,0.05)_30%,_rgba(6,8,17,1)_100%)] w-full h-full absolute top-0 left-0 z-0"></div>
             )}
-            
-            <Image src={item.src} alt={item.alt} height={95} />
+
+            <Image src={item.src} alt={item.alt} height={95} className="z-10" />
           </article>
         ))}
       </div>
@@ -51,4 +56,3 @@ const Techonolgies = () => {
 };
 
 export default Techonolgies;
-
