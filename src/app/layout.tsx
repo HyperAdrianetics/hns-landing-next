00cdba6222d6670deb6e2d958cd7ecb82fba4e28
Gerardo Solis <gsolis@entersolteam.com.mx>
2025-10-24 09:08:22 -0600

    feat: se intregra google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import ParticlesBackground from "./components/ParticlesBackground";
 
@@ -54,8 +55,22 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="es">
       <body className={`${openSans.className} antialiased`}>
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-4XGJE6WLZ1"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-4XGJE6WLZ1');
+          `}
+        </Script>
+
         <ParticlesBackground />
         {children}
       </body>
